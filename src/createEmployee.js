768e34e98a9dab6565f9bb4f0649960d01c6c157
Leonardo BENITEZ <leonardo.benitez@naranja.com>
2023-06-10 02:54:05 -0500

    config serverless: config DB dynamo, config endpoint create employee

---
+++ src/createEmployee.js
@@ -0,0 +1,17 @@
+//const config = require('config');
+//const AWS = require('aws-sdk');
+//const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+module.exports.create = async (event) => {
+    //const USER_AGENT = JSON.stringify(event.headers['User-Agent']);
+    //const PLATFORM = JSON.stringify(event.headers['x-app-platform']);
+
+    //console.log(
+      //  `PLATFORM: ${PLATFORM} - USER-AGENT: ${USER_AGENT}`
+    //);
+
+    return {
+        statusCode: 200,
+        body: 'Ok!!',
+    };
+};
